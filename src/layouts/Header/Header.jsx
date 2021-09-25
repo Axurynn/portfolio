236e7d7d4a918914401a9aaf4bd1f96191d21eaf
Axurynn <marion.bricout@outlook.com>
2021-09-25 18:26:13 +0200

    media queries adjustments for desktop version

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -7,6 +7,7 @@ import { ReactComponent as Linkedin } from "../../assets/linkedin-alt.svg";
 import { ReactComponent as Twitter } from "../../assets/twitter.svg";
 import Avatar from "../../assets/avatar.png";
 import { ReactComponent as Send } from "../../assets/send.svg";
+import { ReactComponent as Scroll } from "../../assets/scroll.svg";
 
 const Header = () => {
   return (
@@ -28,21 +29,42 @@ const Header = () => {
             <img className={s.picture} src={Avatar} alt="avatar" />
           </div>
         </div>
-        <div className={s.content}>
-          <h1 className={s.title}>
-            Hello, <br /> je suis Marion Bricout
-          </h1>
-          <h2 className={s.subtitle}>Développeuse backend</h2>
+        <div className={s.desktopMiddle}>
+          <div className={s.textButton}>
+            <div className={s.content}>
+              <h1 className={s.title}>
+                Hello, <br /> je suis Marion Bricout
+              </h1>
+              <h2 className={s.subtitle}>Développeuse backend</h2>
+              <p className={s.text}>
+                Étant passée depuis peu sur le quai 9{" "}
+                <span>
+                  <sup>3</sup>/<sub>4</sub>
+                </span>
+                , j'ai pu découvrir toute la magie du web. Je souhaite
+                maintenant mettre mes nouvelles compétences au service d'une
+                équipe dynamique et en apprendre davantage.
+              </p>
+            </div>
+            {/* Importe le component "button" qui est paramétrable et réutilisable partout, il suffit de modifier les champs text, Icon et callback (ce qu'il se passe au clic) */}
+            <div className={s.button}>
+              <Button
+                text="Contactez-moi !"
+                Icon={Send}
+                callback={() =>
+                  (window.location.href =
+                    "mailto:marion.bricout@protonmail.com")
+                }
+              />
+            </div>
+          </div>
+          <div className={s.infoScroll}>
+            <Scroll />
+            <p className={s.textScroll}>Défiler vers le bas</p>
+          </div>
         </div>
-        {/* Importe le component "button" qui est paramétrable et réutilisable partout, il suffit de modifier les champs text, Icon et callback (ce qu'il se passe au clic) */}
-        <div className={s.button}>
-          <Button
-            text="Contactez-moi !"
-            Icon={Send}
-            callback={() =>
-              (window.location.href = "mailto:marion.bricout@protonmail.com")
-            }
-          />
+        <div className={s.avatarDesktop}>
+          <img className={s.picture} src={Avatar} alt="avatar" />
         </div>
       </div>
     </>
